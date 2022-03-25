a78c0f329942ae569fbdc5a1f3776432ef33f174
Eldad Danladi <eldaddanladi@gmail.com>
2022-03-25 06:48:13 -0600

    player config

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #define _GLIBCXX_USE_CXX11_ABI 0
 #include <iostream>
 #include <map>
+#include <ctime>
 /**
  /// * Fmt is the C++ formatting library. For more information checkout https://github.com/fmtlib/fmt
 * */
@@ -8,6 +9,7 @@
 /**
  /// * Explicit is where all definitions should go, impliment everything in the header file like a distingushed individual.
 **/
+
 //     std::map<std::string, int> animals = { {"fox", 0},{"sheep",0},{"turtle",0} };
 //     std::cout << "Hello user. You will be prompted ten(10) times to enter the name of an animal." << std::endl;
 //     std::cout << "You are only allowed to enter animals from the list [fox, sheep, turtle]. Thank you." << std::endl;
@@ -34,22 +36,32 @@
 //     std::cout << "sheep  --> " << animals["sheep"] << std::endl;
 //     std::cout << "turtle --> " << animals["turtle"] << std::endl;
 
-int main(){
-    bool game_is_running = true;
-    player.insert(std::pair<std::string, std::string>("attack", "16"));
-    player.insert(std::pair<std::string, std::string>("health", "100"));
-    int player_health = stoi(player["health"]);
+/**
+ * * Trash comments: Put useless/useful code here
+ * player["health"] = std::to_string(player_health);
+   std::cout << "New Player health: " << player["health"] << std::endl;
+ * 
+ *  int player_health = stoi(player["health"]);
     int player_attack = stoi(player["attack"]);
+    int player_heal = stoi(player["heal"]);
+ *    
+ *  std::cout << "Enter new health: ";
+    std::cin >> p.health;
+ */
+
+int main(){
+    bool game_is_running = true; // boolean for main game loop.
+
+    srand(0);
+    uint16_t random = rand() + 1 % 10; // uint stands for unsigned int, this can also be written as `unsigned short random`.
+    
     while(game_is_running != false){
         std::cout << "Enter Player Name: ";
         std::cin >> player["name"];
         std::cout <<"Player name: "<< player["name"] << std::endl;
-        // std::cout << stoi(player["health"]);
-        std::cout << "Health: " << player_health << std::endl;
-        std::cout << "Attack: " << player_attack << std::endl;
-        
+        std::cout << "Health: " << p.health << std::endl;
+        std::cout << "Attack: " << p.attack << std::endl;
         game_is_running = false;
-        
     }
 
     return 0;
--- includes/explicit.hpp
+++ includes/explicit.hpp
@@ -6,12 +6,38 @@
 #include <vector>
 #include <string>
 
-std::map<std::string, std::string> player; // name, attack, health, 
-std::map<std::string, uint16_t> monster; 
+std::map<std::string, std::string> player = {
+    {"name", ""},
+    {"health", "100"},
+    {"attack", "16"},
+    {"heal", "16"},
+}; // name, health, attack, heal, 
+
+std::map<std::string, std::string> monster = {
+    {"name", "Marx"},
+    {"health", "100"},
+    {"attack", "16"},
+    {"heal", "16"},
+}; // name, health, attack, heal,
+
+std::vector<std::string> monster_names = {
+    "Marx",
+    "Bambino",
+    "Kale"
+}; // list of monster names that will be used for monsters per level.
+
+struct Player {
+    int health = stoi(player["health"]);;
+    int attack = stoi(player["attack"]);
+    int heal = stoi(player["heal"]);
+};
+Player p; // struct to interface with player value easier
+
 std::vector<uint8_t> levels;
 
 const char* player_options =
 R"(
 
-)";
-
+)"; //todo: main menu for start of game
+    //todo: add leveling system for user and monster
+    //todo: add powerups with random damage and cool catchphrases.
